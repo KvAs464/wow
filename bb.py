07d69d9c08ee3c34b083c5fbe3327f85dc930511
KvAs464 <angelinamitukovskaa@gmail.com>
2023-04-14 20:17:36 +0300

    бож пон ало

--- bb.py
+++ bb.py
@@ -30,6 +30,12 @@ async def echo(message: types.Message):
         await bot.send_audio(message.chat.id, open('phonk.mp3', 'rb'))
     if message.text =="soul":
         await bot.send_audio(message.chat.id, open('George Michael - Careless Whisper.mp3', 'rb'))
-
+    if message.text =="Shinunoga":
+        await bot.send_audio(message.chat.id, open('Shinunoga.mp3', 'rb'))
+    if message.text =="bbs":
+        await bot.send_audio(message.chat.id, open('bbs.mp3', 'rb'))
+    if message.text =="снов":
+        await bot.send_audio(message.chat.id, open('снов.mp3', 'rb'))
+        
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
